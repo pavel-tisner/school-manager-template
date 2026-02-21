8d85197f2ed41bc3cbddce668aad7918ca2bd894
Tis5813 <pavel.tisner@gmail.com>
2026-02-21 11:09:26 +0100

    wrote initial app fn and started  parsing

--- src/index.js
+++ src/index.js
@@ -1,6 +1,18 @@
-import { parseCommand } from "./command-parser.js";
+import { parseCommand } from './command-parser.js';
+import promptSync from 'prompt-sync';
+const prompt = promptSync();
 
-// This is the entry point of your application. 
+function app() {
+  const command = prompt('Enter command: ');
+  if (command === 'QUIT' || command === 'q') {
+    console.log('Exit');
+    return;
+  }
+
+  parseCommand(command);
+}
+app()
+
+// QUIT or q
+// This is the entry point of your application.
 // Ask user for input, parse the command, and call the appropriate function from courseCommands.js or traineeCommands.js based on the command.
-
-console.log('Hello world');
